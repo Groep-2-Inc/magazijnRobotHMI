da4fbf14c36a3b0c05bec7fc4289ad018c9e3761
JoelleDoorn <s1183785@student.windesheim.nl>
2023-04-21 15:04:23 +0200

    US4_5_SeelecterenEnZoekenOrders
    Compleet maken van US4 en 5

--- src/FrameOrders.java
+++ src/FrameOrders.java
@@ -14,116 +14,137 @@ public class FrameOrders extends FrameHeader implements ActionListener {
     private JComboBox jcb_sort; //combobox voor het sorteren
 
     public FrameOrders(ArrayList<Order> orders){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // programma sluit als op kruisje geklikt wordt
-        setTitle("Java-application/Orders"); // moet nog een betere titel komen lijkt mij
+        //standaard instellingen
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Java-application/Orders"); // moet nog een betere naam hebben
 
         this.orders = orders; // Het attribuut de meegegeven waarde geven
 
-        JPanel panelButtons1 = new JPanel(); // panel voor de buttons
-        panelButtons1.setLayout(null); // panel zetten op geen layout, zodat de plaatsen van de buttons handmatig ingesteld kan worden
+        //panel  voor de buttons aanmaken en de juiste grootte meegeven
+        JPanel panelButtons1 = new JPanel();
+        panelButtons1.setLayout(null);
         panelButtons1.setPreferredSize(new Dimension(1500, 50));
 //        panelButtons1.setBackground(Color.green); // voor het debuggen
 
-        JLabel jl_orderLabel = new JLabel("Orders"); // label voor de orders
-        jl_orderLabel.setFont(new Font(jl_orderLabel.getFont().getName(), jl_orderLabel.getFont().getStyle(), 20)); // lettertype van het label vergoten naar font 20, met dikgedrukte stijl
-        panelButtons1.add(jl_orderLabel); // label toevoegen aan buttonspanel
-        Dimension sizeOrderLabel = jl_orderLabel.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jl_orderLabel.setBounds(10, 10, sizeOrderLabel.width, sizeOrderLabel.height); // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
+        //label voor de tekst orders aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_orderLabel = new JLabel("Orders");
+        jl_orderLabel.setFont(new Font(jl_orderLabel.getFont().getName(), jl_orderLabel.getFont().getStyle(), 20));
+        panelButtons1.add(jl_orderLabel);
+        Dimension sizeOrderLabel = jl_orderLabel.getPreferredSize();
+        jl_orderLabel.setBounds(10, 10, sizeOrderLabel.width, sizeOrderLabel.height);
 
-        JLabel jl_sortLabel = new JLabel("Sortering:"); // label voor de sortering
-//        jl_sortLabel.setFont(new Font("Arial", Font.PLAIN, 10)); // grootte letters kan handmatig ingesteld worden
-        panelButtons1.add(jl_sortLabel); // label toevoegen aan buttonspanel
-        Dimension sizeSortLabel = jl_sortLabel.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jl_sortLabel.setBounds(800, 20, sizeSortLabel.width +10, sizeSortLabel.height); // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
+        //label voor de tekst sortering aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_sortLabel = new JLabel("Sortering:");
+        panelButtons1.add(jl_sortLabel);
+        Dimension sizeSortLabel = jl_sortLabel.getPreferredSize();
+        jl_sortLabel.setBounds(800, 20, sizeSortLabel.width +10, sizeSortLabel.height);
 
-        String sortpossibilities[]={"Ordernummer oplopend", "Ordernummer aflopend", "Datum oplopend", "Datum aflopend", "Voltooid", "Onvoltooid"}; // string aanmaken met de tekst voor in de combobox
-        jcb_sort = new JComboBox(sortpossibilities); // combobox aanmaken met de net aangemaakte string
-        panelButtons1.add(jcb_sort); // label toevoegen aan buttonspanel
-        Dimension sizeSortBox = jcb_sort.getPreferredSize(); // maakt een dimensie aan met de grootte van de sortbox
-        jcb_sort.setBounds(800 + sizeSortLabel.width +10 , 15, sizeSortBox.width, sizeSortBox.height); // zet de plaats van de sortbox en de grootte van de sortbox wordt gebaseerd op de tekst van de sortbox
+        //Combobox aanmaken en waarde toekennen, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        String sortpossibilities[]={"Ordernummer oplopend", "Ordernummer aflopend", "Datum oplopend", "Datum aflopend", "Voltooid", "Onvoltooid"};
+        jcb_sort = new JComboBox(sortpossibilities);
+        panelButtons1.add(jcb_sort);
+        Dimension sizeSortBox = jcb_sort.getPreferredSize();
+        jcb_sort.setBounds(800 + sizeSortLabel.width +10, 15, 175, 30);
 
-        jtf_customerNumber = new JTextField("Klantnummer", 10); // tekstveld voor klantnummer
-        panelButtons1.add(jtf_customerNumber); // tekstveld toevoegen aan buttonspanel
-        Dimension sizeCustomerNumber = jtf_customerNumber.getPreferredSize(); // maakt een dimensie aan met de grootte van het tekstveld
-        jtf_customerNumber.setBounds(1125 , 20, sizeCustomerNumber.width, sizeCustomerNumber.height); // zet de plaats van het tekstveld en de grootte van het tekstveld wordt gebaseerd op de tekst in het tekstveld
+        //Tekstveld voor klantnummer aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        jtf_customerNumber = new JTextField("Klantnummer", 10);
+        panelButtons1.add(jtf_customerNumber);
+        Dimension sizeCustomerNumber = jtf_customerNumber.getPreferredSize();
+        jtf_customerNumber.setBounds(1070, 15, 120, 30);
 
-        jtf_orderNumber = new JTextField("Ordernummer", 10); // tekstveld voor ordernummer
-        panelButtons1.add(jtf_orderNumber); // tekstveld toevoegen aan buttonspanel
-        Dimension sizeOrderNumber = jtf_orderNumber.getPreferredSize(); // maakt een dimensie aan met de grootte van het tekstveld
-        jtf_orderNumber.setBounds(1250 , 20, sizeOrderNumber.width, sizeOrderNumber.height);  // zet de plaats van het tekstveld en de grootte van het tekstveld wordt gebaseerd op de tekst in het tekstveld
+        //Tekstveld voor ordernummer aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        jtf_orderNumber = new JTextField("Ordernummer", 10);
+        panelButtons1.add(jtf_orderNumber);
+        Dimension sizeOrderNumber = jtf_orderNumber.getPreferredSize();
+        jtf_orderNumber.setBounds(1200, 15, 120, 30);
 
-        jb_search = new JButton("Zoeken"); // button voor zoeken
-        panelButtons1.add(jb_search); // button toevoegen aan buttonspanel
+        // Buttun voor zoeken aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        jb_search = new JButton("Zoeken");
+        jb_search.setFont(new Font("Arial", Font.PLAIN, 17));
+        panelButtons1.add(jb_search);
         jb_search.addActionListener(this); // actionlistener toevoegen aan button
-        Dimension sizeSearchButton = jb_search.getPreferredSize(); // maakt een dimensie aan met de grootte van de button
-        jb_search.setBounds(1400 , 15, sizeSearchButton.width, sizeSearchButton.height); // zet de plaats van de button en de grootte van het button wordt gebaseerd op de tekst in de button
+        Dimension sizeSearchButton = jb_search.getPreferredSize();
+        jb_search.setBounds(1350, 10, 120, 40);
 
         super.add(panelButtons1); // panelButtons toevoegen aan het hoofdscherm
 
+        //Panel aanmaken voor de titels, juiste grootte en kleur meegeven
+        JPanel panelTitles = new JPanel();
+        panelTitles.setLayout(null);
+        panelTitles.setPreferredSize(new Dimension(1500, 40));
+        panelTitles.setBackground(Color.lightGray);
 
-        JPanel panelTitles = new JPanel(); // panel aanmaken voor de titels van de scrollbar
-        panelTitles.setLayout(null); // panel zetten op geen layout, zodat labels handmatig op juiste plek gezet kunnen worden
-        panelTitles.setPreferredSize(new Dimension(1500, 40)); // grootte van het panel instellen
-        panelTitles.setBackground(Color.lightGray); // voor het debuggen kleur toevoegen
+        //label voor order aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_orderText = new JLabel("Order");
+        jl_orderText.setFont(new Font("Arial", Font.PLAIN, 20));
+        panelTitles.add(jl_orderText);
+        Dimension sizeOrderText = jl_orderText.getPreferredSize();
+        jl_orderText.setBounds(50, 10, sizeOrderText.width, sizeOrderText.height);
 
+        //label voor naam aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_customerText = new JLabel("Naam");
+        jl_customerText.setFont(new Font("Arial", Font.PLAIN, 20));
+        panelTitles.add(jl_customerText);
+        Dimension sizeCustomerText = jl_customerText.getPreferredSize();
+        jl_customerText.setBounds(400, 10, sizeCustomerText.width +10, sizeCustomerText.height);
 
-        JLabel jl_orderText = new JLabel("src.Order"); // label voor de order
-        jl_orderText.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype instellen op font 20
-        panelTitles.add(jl_orderText); // label toevoegen aan het panel voor de titels
-        Dimension sizeOrderText = jl_orderText.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jl_orderText.setBounds(50, 10, sizeOrderText.width, sizeOrderText.height);  // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
+        //label voor producten aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_productsText = new JLabel("Producten");
+        jl_productsText.setFont(new Font("Arial", Font.PLAIN, 20));
+        panelTitles.add(jl_productsText);
+        Dimension sizeProductenText = jl_productsText.getPreferredSize();
+        jl_productsText.setBounds(750, 10, sizeProductenText.width +10, sizeProductenText.height);
 
-        JLabel jl_customerText = new JLabel("Naam"); // label voor de naam
-        jl_customerText.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype instellen op font 20
-        panelTitles.add(jl_customerText); // label toevoegen aan panel van de titels
-        Dimension sizeCustomerText = jl_customerText.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jl_customerText.setBounds(400, 10, sizeCustomerText.width +10, sizeCustomerText.height); // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
+        //label voor producten aantal aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jl_productsQuantityText = new JLabel("(aantal)");
+        jl_productsQuantityText.setFont(new Font("Arial", Font.PLAIN, 15));
+        panelTitles.add(jl_productsQuantityText);
+        Dimension sizeProductsQuantityText = jl_productsQuantityText.getPreferredSize();
+        jl_productsQuantityText.setBounds(750 +sizeProductenText.width +10, 15, sizeProductsQuantityText.width +10, sizeProductsQuantityText.height);
 
-        JLabel jl_productenText = new JLabel("Producten"); // label voor de producten
-        jl_productenText.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype instellen op font 20
-        panelTitles.add(jl_productenText); // label toevoegen aan panel van de titels
-        Dimension sizeProductenText = jl_productenText.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jl_productenText.setBounds(750, 10, sizeProductenText.width +10, sizeProductenText.height); // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
-
-        JLabel jlDateText = new JLabel("Datum"); // label voor de datum
-        jlDateText.setFont(new Font("Arial", Font.PLAIN, 20));  // lettertype instellen op font 20
-        panelTitles.add(jlDateText); // label toevoegen aan panel van de titels
-        Dimension sizeDateText = jlDateText.getPreferredSize(); // maakt een dimensie aan met de grootte van het label
-        jlDateText.setBounds(1100, 10, sizeDateText.width +10, sizeDateText.height); // zet de plaats van het label en de grootte van het label wordt gebaseerd op de tekst in het label
-
+        //label voor Datum aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        JLabel jlDateText = new JLabel("Datum");
+        jlDateText.setFont(new Font("Arial", Font.PLAIN, 20));
+        panelTitles.add(jlDateText);
+        Dimension sizeDateText = jlDateText.getPreferredSize();
+        jlDateText.setBounds(1100, 10, sizeDateText.width +10, sizeDateText.height);
 
         super.add(panelTitles); // panel van de titels toevoegen aan het hoofdscherm
 
-        JPanel panel2 = new JPanel(); // panel aanmaken
-        panel2.setLayout(new FlowLayout()); // panel instllen op flowlayout
-        panel2.setPreferredSize(new Dimension(1500, 100* orders.size() + 50)); // grootte van dit panel instellen
+        //panel aanmaken, waar het scrollpanel inkomt
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout());
+        panel2.setPreferredSize(new Dimension(1500, 100* orders.size() + 50));
 
-        int teller = 0; // variable teller
-        for (Order order : orders) { // for loop, om de orders te laten doorlopen
-            JPanel panelOrders = new OrderPanel(order); //panel aanmaken voor de orders
-            panel2.add(panelOrders); //panel voor de orders toevoegen aan panel2
-            Dimension sizeOrderPanel = panelOrders.getPreferredSize(); // grootte van het panel bepalen
-            panelOrders.setBounds(0, sizeOrderPanel.height * teller, sizeOrderPanel.width, sizeOrderPanel.height); // de panels achterelkaar plaatsen
-            teller ++; // de teller optellen
+        //for loop waarbij voor elke order nieuw panel wordt aangemaakt, de teller wordt opgehoogt en de grootte en de plaats van elk panel wordt bedacht
+        int teller = 0;
+        for (Order order : orders) {
+            JPanel panelOrders = new OrderPanel(order);
+            panel2.add(panelOrders);
+            Dimension sizeOrderPanel = panelOrders.getPreferredSize();
+            panelOrders.setBounds(0, sizeOrderPanel.height * teller, sizeOrderPanel.width, sizeOrderPanel.height);
+            teller ++;
         }
 
+        //aanmaken scrollpane, juiste grootte megeven en de vertical scrollbar en toevoegen aan het scherm
+        JScrollPane scrollPane = new JScrollPane(panel2);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(1500, 535));
+        super.add(scrollPane);
 
-        JScrollPane scrollPane = new JScrollPane(panel2); // aanmaken scrollPane en toevoegn aan penl 2
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // hierdoor wordt nooit een horizontale scrollbar toegevoegd
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // altijd standaard een scrollbar toevoegen
-        scrollPane.setPreferredSize(new Dimension(1500, 535)); // de grootte van het scrollpane bepalen
-        super.add(scrollPane); // scrollpane toevoegen aan de hoofdpagina
-
-        JPanel panel3 = new JPanel(); // panel aanmaken
-        panel3.setLayout(null); // panel zetten op geen layout voor het handmatig instellen van de locaties
-        panel3.setPreferredSize(new Dimension(1500, 50)); // de grootte van het pannel instellen
+        //aanmaken derde panel voor inzien button, layout op null zetten
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(null);
+        panel3.setPreferredSize(new Dimension(1500, 50));
 //        panel3.setBackground(Color.magenta); // voor het debuggen
 
-        jb_view = new JButton("Inzien"); // jbutton aanmaken voor inzien
-        panel3.add(jb_view); // button toevoegen aan panel 3
+        //Button voor inzien aanmaken, toevoegen aan panel en de juiste plek, grootte en lettertype meegeven
+        jb_view = new JButton("Inzien");
+        panel3.add(jb_view);
         jb_view.setFont(new Font("Arial", Font.PLAIN, 17));
-        Dimension sizeViewButton = jb_view.getPreferredSize(); // de grootte van het scrollpane bepalen
-        jb_view.setBounds(1400, 10, 150, 40); // de grootte bepalen aan de hand van de grootte van de tekst in de button
+        Dimension sizeViewButton = jb_view.getPreferredSize();
+        jb_view.setBounds(1350, 10, 120, 40);
         jb_view.addActionListener(this); // actionlistener toevoegen
 
         super.add(panel3); // panel3 toevoegen aan het hoofdscherm
--- src/OrderPanel.java
+++ src/OrderPanel.java
@@ -11,45 +11,49 @@ public class OrderPanel extends JPanel {
 
     public OrderPanel(Order order) {
         this.order = order; // het attribuut krijgt de de meegegeven waarde
-        setPreferredSize(new Dimension(1500, 100)); // de grootte van het panel instellen
-        setBackground(Color.white); // de kleur wit instellen
-        setLayout(null); // geen layout instellen, voor het handmatig instellen voor de locaties
+        //standaardinstelling: grootte, kleur en juiste layout(geen) meegegeven
+        setPreferredSize(new Dimension(1500, 100));
+        setBackground(Color.white);
+        setLayout(null);
 
+        //label aanmaken voor de order, int waarde omgezet naar string en juiste lettertype, grootte en plaats meegegeven
+        String stringOrderID = valueOf(this.order.getOrderID());
+        JLabel jlOrderID = new JLabel(stringOrderID);
+        jlOrderID.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(jlOrderID);
+        Dimension sizeOrderID = jlOrderID.getPreferredSize();
+        jlOrderID.setBounds(50, 40, sizeOrderID.width, sizeOrderID.height);
 
-        String stringOrderID = valueOf(this.order.getOrderID()); // de int orderID omzetten naar string
-        JLabel jlOrderID = new JLabel(stringOrderID); // label aanmaken met de net aangemaakte string
-        jlOrderID.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype font 20 instellen
-        add(jlOrderID); // toevoegen label aan panel
-        Dimension sizeOrderID = jlOrderID.getPreferredSize(); // de grootte van het label bepalen
-        jlOrderID.setBounds(50, 40, sizeOrderID.width, sizeOrderID.height); // de locatie van het label instellen, gebaseerd op de grootte van de tekst in het label
+        //label aanmaken voor de  naam en juiste lettertype, grootte en plaats meegegeven
+        JLabel jlCustomerName = new JLabel(this.order.getCustomer().getCustomername());
+        jlCustomerName.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(jlCustomerName);
+        Dimension sizeCustomerName = jlCustomerName.getPreferredSize();
+        jlCustomerName.setBounds(400, 40, sizeCustomerName.width + 10, sizeCustomerName.height);
 
-        JLabel jlCustomerName = new JLabel(this.order.getCustomer().getCustomername()); // label aanmaken met de klantnaam
-        jlCustomerName.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype instellen op font 20
-        add(jlCustomerName); // toevoegn label aan panel
-        Dimension sizeCustomerName = jlCustomerName.getPreferredSize(); // de grootte van het label bepalen
-        jlCustomerName.setBounds(400, 40, sizeCustomerName.width + 10, sizeCustomerName.height); // de locatie van het label instellen, gebaseerd op de grootte van de tekst in het label
+        //label aanmaken voor de custumerID, int waarde omgezet naar string en juiste lettertype, grootte en plaats meegegeven
+        JLabel jlCustomerID = new JLabel(", " + this.order.getCustomer().getCustomerID());
+        jlCustomerID.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(jlCustomerID);
+        Dimension sizeCustomerID = jlCustomerID.getPreferredSize();
+        jlCustomerID.setBounds(400 + sizeCustomerName.width + 2, 40, sizeCustomerID.width + 10, sizeCustomerID.height);
 
-        JLabel jlCustomerID = new JLabel(", " + this.order.getCustomer().getCustomerID()); // label toevoegn voor het klant ID
-        jlCustomerID.setFont(new Font("Arial", Font.PLAIN, 20)); // instel0en van lettertype font 29
-        add(jlCustomerID); // toevoegen label aan panel
-        Dimension sizeCustomerID = jlCustomerID.getPreferredSize(); // de grootte van het label bepalen
-        jlCustomerID.setBounds(400 + sizeCustomerName.width + 2, 40, sizeCustomerID.width + 10, sizeCustomerID.height); // de locatie van het label instellen, gebaseerd op de grootte van de tekst in het label
+        //label aanmaken voor de producten, int waarde omgezet naar string en juiste lettertype, grootte en plaats meegegeven
+        String stringProductAmount = valueOf(this.order.getProductAmount());
+        JLabel jlProductAmount = new JLabel(stringProductAmount);
+        jlProductAmount.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(jlProductAmount);
+        Dimension sizeProductAmount = jlProductAmount.getPreferredSize();
+        jlProductAmount.setBounds(750, 40, sizeProductAmount.width, sizeProductAmount.height);
 
-
-        String stringProductAmount = valueOf(this.order.getProductAmount()); // de int aantal producten omzetten naar string
-        JLabel jlProductAmount = new JLabel(stringProductAmount); // label toevoegen met de net omgezette string
-        jlProductAmount.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype op font 20 zetten
-        add(jlProductAmount); // toevoegen label aan panel
-        Dimension sizeProductAmount = jlProductAmount.getPreferredSize(); // de grootte van het label bepalen
-        jlProductAmount.setBounds(750, 40, sizeProductAmount.width, sizeProductAmount.height); // de locatie van het label instellen, gebaseerd op de grootte van de tekst in het label
-
-        Date date = this.order.getDate(); // variable datum omzetten
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm"); // datumformat instellen met patronen
-        String strDate = dateFormat.format(date); // datum omvormen naar juist formaat
-        JLabel jlDate = new JLabel(strDate); // label aanmaken met de string datum
-        jlDate.setFont(new Font("Arial", Font.PLAIN, 20)); // lettertype op font 20 zetten
-        add(jlDate); // toevoegen label aan panel
-        Dimension sizeDate = jlDate.getPreferredSize(); // de grootte van het label bepalen
-        jlDate.setBounds(1100, 40, sizeDate.width, sizeDate.height); // de locatie van het label instellen, gebaseerd op de grootte van de tekst in het label
+        //label aanmaken voor de datum, datum converteren naar bepaald patroon en juiste lettertype, grootte en plaats meegeven
+        Date date = this.order.getDate();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+        String strDate = dateFormat.format(date);
+        JLabel jlDate = new JLabel(strDate);
+        jlDate.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(jlDate);
+        Dimension sizeDate = jlDate.getPreferredSize();
+        jlDate.setBounds(1100, 40, sizeDate.width, sizeDate.height);
     }
 }
