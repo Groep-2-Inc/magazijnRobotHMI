d4b401333ce42851e397a31ec2d0a30fe3629b74
martijnschuman <martijnschuman@hotmail.com>
2023-04-22 20:18:43 +0200

    Comms demo

--- HMI/src/Main.java
+++ HMI/src/Main.java
@@ -1,7 +1,14 @@
 import frames.FrameHome;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new FrameHome();
+
+        Communication coms = new Communication();
+        coms.sendComms(200);
+
+        coms.getComms();
     }
 }
