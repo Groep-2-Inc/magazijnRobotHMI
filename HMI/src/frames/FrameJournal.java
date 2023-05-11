53265ffacf39469499fff356bfd63f1443aa9bc6
JoelleDoorn <s1183785@student.windesheim.nl>
2023-05-11 15:42:52 +0200

    US13-logboek

--- HMI/src/frames/FrameJournal.java
+++ HMI/src/frames/FrameJournal.java
@@ -2,6 +2,7 @@ package frames;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -15,9 +16,10 @@ import classes.*;
 public class FrameJournal extends FrameHeader {
     private ArrayList<Activity> activities = new ArrayList<>(); // lijst met activeiten (Joëlle)
     private JLabel jl_journal =new JLabel("Logboek");
+    private JScrollPane scrollPane;
+    private JPanel panel;
 
     public FrameJournal() {
-        getLogbookData();
         closeProgram();
 
         setTitle("Java-application/Logboek"); // moet nog een betere titel komen lijkt mij (Joëlle)
@@ -31,65 +33,109 @@ public class FrameJournal extends FrameHeader {
         jl_journal.setFont(new Font("Arial", Font.BOLD, 30));
         panelJournalTitle.add(jl_journal);
         Dimension sizeJournalText = jl_journal.getPreferredSize();
-        jl_journal.setBounds(getScreenWidth(0f), getScreenHeight(1.157407407f), sizeJournalText.width +10, sizeJournalText.height);
+        jl_journal.setBounds(getScreenWidth(0f), getScreenHeight(1.157407407f), sizeJournalText.width + 10, sizeJournalText.height);
 
         super.add(panelJournalTitle);
-
+        getLogbookData();
         //Panel aanmaken voor waar de scrollpane inkomt (Joëlle)
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setPreferredSize(new Dimension(getScreenWidth(98f), 16 * activities.size())); //procenten toegevoegd, bij height mist nog, kan later evt. toegevoegd worden(Joëlle)
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setPreferredSize(new Dimension(getScreenWidth(98f), 46 * activities.size())); //procenten toegevoegd, bij height mist nog, kan later evt. toegevoegd worden(Joëlle)
+
 
         //Een for-loop, doorloopt de activiteiten en print de datum en beschrijving (Joëlle)
         for (int i = 0; i < activities.size(); i++) {
-            JLabel label = new JLabel();
+            JPanel panelLogging = new JPanel();
+            panelLogging.setLayout(new GridLayout(1, 1));
+            panelLogging.setPreferredSize(new Dimension(getScreenWidth(98f), 40));
+            panelLogging.setBorder(new LineBorder(Color.BLACK));
             Date date = activities.get(i).getDate(); // converteren van de datum naar string
             DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm");
             String strDate = dateFormat.format(date);
 
-            label.setText(strDate + " ID:" + activities.get(i).getId() + "type: " + activities.get(i).getTypeText() + " - " + activities.get(i).getActivityDescription());
-            panel.add(label);
+            panelLogging.add(new JLabel(strDate + " ID:" + activities.get(i).getId() + " type: " + activities.get(i).getTypeText() + " - " + activities.get(i).getActivityDescription()));
+            panel.add(panelLogging);
         }
 
         //Aanmaken scrollpane, met vertical scrollbar plus juiste grootte meegegeven (Joëlle)
-        JScrollPane scrollPane = new JScrollPane(panel); // aanmaken scrollPane
+        scrollPane = new JScrollPane(panel); // aanmaken scrollPane
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.getVerticalScrollBar().setUnitIncrement(14);
         scrollPane.setPreferredSize(new Dimension(getScreenWidth(98f), getScreenHeight(70f))); //procenten toegevoegd (Joëlle)
 
         super.add(scrollPane); // scrollpane toevoegen aan de hoofdpagina (Joëlle)
+        PanelinJournal();
+    }
+    private void PanelinJournal(){
+        getLogbookData();
+        //Panel aanmaken voor waar de scrollpane inkomt (Joëlle)
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setPreferredSize(new Dimension(getScreenWidth(98f), 46 * activities.size())); //procenten toegevoegd, bij height mist nog, kan later evt. toegevoegd worden(Joëlle)
+
+
+        //Een for-loop, doorloopt de activiteiten en print de datum en beschrijving (Joëlle)
+        for (int i = 0; i < activities.size(); i++) {
+            JPanel panelLogging = new JPanel();
+            panelLogging.setLayout(new GridLayout(1, 1));
+            panelLogging.setPreferredSize(new Dimension(getScreenWidth(98f), 40));
+            panelLogging.setBorder(new LineBorder(Color.BLACK));
+            Date date = activities.get(i).getDate(); // converteren van de datum naar string
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+            String strDate = dateFormat.format(date);
+
+            panelLogging.add(new JLabel(strDate + " ID:" + activities.get(i).getId() + " type: " + activities.get(i).getTypeText() + " - " + activities.get(i).getActivityDescription()));
+            panel.add(panelLogging);
+        }
+
+        //Aanmaken scrollpane, met vertical scrollbar plus juiste grootte meegegeven (Joëlle)
+        scrollPane = new JScrollPane(panel); // aanmaken scrollPane
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(14);
+        scrollPane.setPreferredSize(new Dimension(getScreenWidth(98f), getScreenHeight(70f))); //procenten toegevoegd (Joëlle)
+
+        super.add(scrollPane); // scrollpane toevoegen aan de hoofdpagina (Joëlle)
+        redraw();
     }
 
     private void getLogbookData() {
         JSONArray allActivities;
-
-        // Maakt de basis query, wat bij elke query hetzelfde is (Joëlle)
-        String baseQuery = "SELECT id, type, text, date FROM logbook ORDER BY id DESC";
+        activities.clear();
 
         // Haalt alle data op en zet deze in de array (Joëlle)
-        allActivities = Database.getDbData(baseQuery, new String[]{});
+        allActivities = Database.getDbData("SELECT id, type, text, date FROM logbook ORDER BY id DESC", new String[]{});
 
         for(Object singelLogbookData: allActivities){
-            // Zet het Object om naar een JSON-object
+            // Zet het Object om naar een JSON-object (Joëlle)
             JSONObject logbookData = (JSONObject) singelLogbookData;
 
-            // Maak een nieuwe default date aan
+            // Maak een nieuwe default date aan (Joëlle)
             Date logbookDate = new Date(2013, 1, 1);
             try{
-                // Probeer een nieuwe date format aan te maken
+                // Probeer een nieuwe date format aan te maken (Joëlle)
                 DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                // Zet de String uit de order om naar een Date object
+                // Zet de String uit de order om naar een Date object (Joëlle)
                 logbookDate = sdf.parse(String.valueOf(logbookData.get("date")));
             }catch (ParseException pe){
                 System.out.println(getClass() + "Framelogbook(): " + pe);
             }
 
-            // Maak een nieuwe Activiteit aan met data uit het logbook
+            // Maak een nieuwe Activiteit aan met data uit het logbook (Joëlle)
             Activity activity = new Activity(Integer.parseInt((String) logbookData.get("id")), Integer.parseInt((String) logbookData.get("type")), logbookDate, String.valueOf(logbookData.get("text")));
 
-            //voeg de activiteit toe aan de activiteiten ArrayList
+            //voeg de activiteit toe aan de activiteiten ArrayList (Joëlle)
             activities.add(activity);
         }
+    }
+    // Verwijderd de elementen uit de JFrame en tekent ze opnieuw
+    // Door Martijn
+    public void redraw(){
+        // Verwijder de huidge panelTabel en scrollPane
+        super.remove(panel);
+        super.remove(scrollPane);
+        // Tekent het paneel opnieuw
+        PanelinJournal();
     }
 }
--- HMI/src/panels/PanelLogboek.java
+++ HMI/src/panels/PanelLogboek.java
@@ -3,12 +3,54 @@ package panels;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
+import classes.Activity;
+import database.Database;
 import frames.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 //door Jason Joshua van der Kolk
 public class PanelLogboek extends JPanel{
+    private ArrayList<Activity> activities = new ArrayList<>(); // lijst met activeiten (Joëlle)
     private Font arial14B = new Font("Arial", Font.BOLD, 14);
+    private void getLogbookData() {
+        JSONArray allActivities;
+        activities.clear();
+
+
+        // Haalt alle data op en zet deze in de array (Joëlle)
+        allActivities = Database.getDbData("SELECT id, type, text, date FROM logbook ORDER BY id DESC LIMIT 10", new String[]{});
+
+        for (Object singelLogbookData : allActivities) {
+            // Zet het Object om naar een JSON-object (Joëlle)
+            JSONObject logbookData = (JSONObject) singelLogbookData;
+
+            // Maak een nieuwe default date aan (Joëlle)
+            Date logbookDate = new Date(2013, 1, 1);
+            try {
+                // Probeer een nieuwe date format aan te maken (Joëlle)
+                DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                // Zet de String uit de order om naar een Date object (Joëlle)
+                logbookDate = sdf.parse(String.valueOf(logbookData.get("date")));
+            } catch (ParseException pe) {
+                System.out.println(getClass() + "Framelogbook(): " + pe);
+            }
+
+            // Maak een nieuwe Activiteit aan met data uit het logboek (Joëlle)
+            Activity activity = new Activity(Integer.parseInt((String) logbookData.get("id")), Integer.parseInt((String) logbookData.get("type")), logbookDate, String.valueOf(logbookData.get("text")));
+
+            //voeg de activiteit toe aan de activiteiten ArrayList (Joëlle)
+            activities.add(activity);
+        }
+    }
     public PanelLogboek(){
+        getLogbookData();
         //initializeer het hoofd paneel
         setLayout(new FlowLayout());
         setPreferredSize(new Dimension(FrameHeader.getScreenWidth(50f),FrameHeader.getScreenHeight(50f)));
@@ -28,15 +70,20 @@ public class PanelLogboek extends JPanel{
         //nieuw paneel voor alle informatie
         JPanel p = new JPanel();
         p.setLayout(new FlowLayout());
-        p.setPreferredSize(new Dimension(FrameHeader.getScreenWidth(FrameHeader.getPercentage(1920, 896)), 580)); //TODO hoogte schalen met de hoeveelheid dingen die we erin stoppen
+        p.setPreferredSize(new Dimension(FrameHeader.getScreenWidth(FrameHeader.getPercentage(1920, 896)), 46 * activities.size())); //hoogte geschaald met hoeveelheid(Joëlle)
 
-        //voeg de informatie toe
+        //voegt panels toen, met de gegevens van de database daarin (Joëlle)
         for(int i = 0; i<10; i++){
             JPanel p2 = new JPanel();
             p2.setLayout(new GridLayout(1, 1));
-            p2.setPreferredSize(new Dimension(FrameHeader.getScreenWidth(FrameHeader.getPercentage(1920, 896)), 58));
+            p2.setPreferredSize(new Dimension(FrameHeader.getScreenWidth(FrameHeader.getPercentage(1920, 896)), 40));
             p2.setBorder(new LineBorder(Color.BLACK));
-            p2.add(new JLabel("tekst"));
+
+            Date date = activities.get(i).getDate(); // converteren van de datum naar string
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+            String strDate = dateFormat.format(date);
+
+            p2.add(new JLabel(strDate + " ID:" + activities.get(i).getId() + " type: " + activities.get(i).getTypeText() + " - " + activities.get(i).getActivityDescription()));
             p.add(p2);
         }
 
